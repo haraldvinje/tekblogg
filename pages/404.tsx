004b7eef0fa9b520ca46cd537a4628739d1a2c85
Harald Vinje <harald@xperitech.com>
2022-05-15 10:21:36 +0200

    Change 404 site

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ const Custom404 = () => {
   return (
     <main>
       <h1 className="mb-4 text-xl font-bold text-black">
-        404 - That page does not seem to exist...
+        404 - Siden finnes ikke
       </h1>
     </main>
   )
