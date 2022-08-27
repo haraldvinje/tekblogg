ff04a8250fce895bae0fb62fda19f9069c091bf2
Harald Vinje <harald@xperitech.com>
2022-08-27 13:38:35 +0200

    AnimationLayout->AnimationWrapper, which is more correct

--- personal-blog-web/src/components/layouts/BaseLayout.tsx
+++ personal-blog-web/src/components/layouts/BaseLayout.tsx
@@ -1,14 +1,14 @@
 import { ReactNode } from 'react'
-import AnimationLayout from 'src/components/layouts/AnimationLayout'
+import AnimationWrapper from 'src/components/AnimationWrapper'
 import Navbar from 'src/components/Navbar'
 
 const BaseLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <Navbar />
-      <AnimationLayout>
+      <AnimationWrapper>
         <main className="my-10 flex justify-center py-[5%] px-[10%] xl:px-[20%]">{children}</main>
-      </AnimationLayout>
+      </AnimationWrapper>
     </>
   )
 }
