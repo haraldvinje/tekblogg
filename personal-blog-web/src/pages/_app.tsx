da0636d4e9925127f23519299f53b2b1adbddd29
Harald Vinje <haraldvinje@gmail.com>
2022-10-11 12:29:25 +0200

    Latest

--- personal-blog-web/src/pages/_app.tsx
+++ personal-blog-web/src/pages/_app.tsx
@@ -1,23 +1,19 @@
 import '/styles/globals.css'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
 import type { AppProps } from 'next/app'
-import { Html, Head } from 'next/document'
 import { ThemeProvider } from 'next-themes'
 import BaseLayout from 'src/components/layouts/BaseLayout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Html lang="nb">
-      <Head />
-      <body>
-        <GoogleAnalytics trackPageViews />
-        <ThemeProvider enableSystem={false}>
-          <BaseLayout>
-            <Component {...pageProps} />
-          </BaseLayout>
-        </ThemeProvider>
-      </body>
-    </Html>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <ThemeProvider enableSystem={false}>
+        <BaseLayout>
+          <Component {...pageProps} />
+        </BaseLayout>
+      </ThemeProvider>
+    </>
   )
 }
 
