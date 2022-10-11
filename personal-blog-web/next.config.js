6303e5e6a7c0cb3497b753e2ecedcfa4af45a355
Harald Vinje <haraldvinje@gmail.com>
2022-10-11 10:24:35 +0200

    Adding PWA support

--- personal-blog-web/next.config.js
+++ personal-blog-web/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['cdn.sanity.io', 'nextjs.gallery']
   },
@@ -9,4 +11,14 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching
+})
+
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [withPWA]
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig })
+}
