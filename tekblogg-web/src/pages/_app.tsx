37c49bb3126efc674c72e1d907fd1a28a5faf8f1
Harald Vinje <haraldvinje@gmail.com>
2022-12-08 14:49:40 +0100

    Using partytown

--- tekblogg-web/src/pages/_app.tsx
+++ tekblogg-web/src/pages/_app.tsx
@@ -9,9 +9,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
+        strategy="worker"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" strategy="worker">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
