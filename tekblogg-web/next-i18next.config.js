82245d6b0acae7f8c7cb0ccae846936d3fc17c3f
Harald Vinje <haraldvinje@gmail.com>
2022-12-09 17:00:29 +0100

    Multiple domains

--- tekblogg-web/next-i18next.config.js
+++ tekblogg-web/next-i18next.config.js
@@ -2,6 +2,16 @@ module.exports = {
   debug: process.env.NODE_ENV === 'development',
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'nb']
+    locales: ['en', 'nb'],
+    domains: [
+      {
+        domain: 'tekblogg.dev',
+        defaultLocale: 'nb'
+      },
+      {
+        domain: 'tekblog.dev',
+        defaultLocale: 'en'
+      }
+    ]
   }
 }
