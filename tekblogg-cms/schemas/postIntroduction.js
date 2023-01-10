aac9be7ccbfdba61674140d75157855458be5f55
Harald Vinje <haraldvinje@gmail.com>
2023-01-10 11:01:21 +0100

    Cleanup in cms folder

--- tekblogg-cms/schemas/postIntroduction.js
+++ tekblogg-cms/schemas/postIntroduction.js
@@ -1,45 +1,41 @@
-
 export default {
-  title: 'Introduction',
-  name: 'postIntroduction',
-  type: 'array',
+  title: "Introduction",
+  name: "postIntroduction",
+  type: "array",
   of: [
     {
-      title: 'Block',
-      type: 'block',
+      title: "Block",
+      type: "block",
       // Styles let you set what your user can mark up blocks with. These
       // correspond with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
       // use your content.
-      styles: [
-        {title: 'Normal', value: 'normal'},
-      ],
-      lists: [{title: 'Bullet', value: 'bullet'}],
+      styles: [{ title: "Normal", value: "normal" }],
+      lists: [{ title: "Bullet", value: "bullet" }],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
         // preference or highlighting by editors.
         decorators: [
-          {title: 'Strong', value: 'strong'},
-          {title: 'Emphasis', value: 'em'},
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
-            title: 'URL',
-            name: 'link',
-            type: 'object',
+            title: "URL",
+            name: "link",
+            type: "object",
             fields: [
               {
-                title: 'URL',
-                name: 'href',
-                type: 'url',
+                title: "URL",
+                name: "href",
+                type: "url",
               },
             ],
           },
         ],
       },
     },
-    // as a block type.
   ],
-}
+};
