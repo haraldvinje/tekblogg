b637613a86b832917a6e6ac456353f82516442e9
Harald Vinje <haraldvinje@gmail.com>
2023-01-10 11:06:37 +0100

    Changing actions and testing

--- tekblogg-cms/schemas/postIntroduction.js
+++ tekblogg-cms/schemas/postIntroduction.js
@@ -6,21 +6,13 @@ export default {
     {
       title: "Block",
       type: "block",
-      // Styles let you set what your user can mark up blocks with. These
-      // correspond with HTML tags, but you can set any title or value
-      // you want and decide how you want to deal with it where you want to
-      // use your content.
       styles: [{ title: "Normal", value: "normal" }],
       lists: [{ title: "Bullet", value: "bullet" }],
-      // Marks let you mark up inline text in the block editor.
       marks: {
-        // Decorators usually describe a single property – e.g. a typographic
-        // preference or highlighting by editors.
         decorators: [
           { title: "Strong", value: "strong" },
           { title: "Emphasis", value: "em" },
         ],
-        // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
           {
             title: "URL",
