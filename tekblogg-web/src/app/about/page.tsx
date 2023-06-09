b4b5ae036ad87a7b88c5c282942a27244f2b4c79
Harald Vinje <haraldvinje@gmail.com>
2023-06-09 14:11:01 +0200

    Fixing last canonical

--- tekblogg-web/src/app/about/page.tsx
+++ tekblogg-web/src/app/about/page.tsx
@@ -7,7 +7,10 @@ const title = 'Om TekBlogg'
 export const metadata: Metadata = {
   title,
   description:
-    'TekBlogg er en blogg om teknologi og programmering. Bloggen er drevet av Harald Vinje.'
+    'TekBlogg er en blogg om teknologi og programmering. Bloggen er drevet av Harald Vinje.',
+  alternates: {
+    canonical: '/about'
+  }
 }
 
 export default async function AboutPage() {
