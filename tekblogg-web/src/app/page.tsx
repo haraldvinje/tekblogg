ea1fa0b8783e0ec46f9159cd1f2d0e3152abafa3
Harald Vinje <haraldvinje@gmail.com>
2023-07-22 20:45:58 +0200

    Revalidate home page

--- tekblogg-web/src/app/page.tsx
+++ tekblogg-web/src/app/page.tsx
@@ -2,6 +2,8 @@ import { AnimationWrapper } from '@/components/animation-wrapper'
 import { getAllBlogPostsMetadata } from '@/lib/sanity-client'
 import { Home } from './home'
 
+export const revalidate = 60
+
 export default async function Page() {
   const posts = await getAllBlogPostsMetadata()
   return (
