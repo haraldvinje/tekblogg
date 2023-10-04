a0c169c9965e1978b4c443091b88a43849186064
Harald Vinje <haraldvinje@gmail.com>
2023-10-04 12:28:14 +0200

    Fixing icons on share button

--- tekblogg-web/src/components/share-buttons.tsx
+++ tekblogg-web/src/components/share-buttons.tsx
@@ -2,13 +2,14 @@
 
 import { faLinkedin, faSquareXTwitter } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquare } from '@fortawesome/free-solid-svg-icons'
 import { useAbsoluteUrl } from '@/lib/hooks/use-absolute-url'
 
 export function ShareButtons({ className }: { className?: string }) {
   const url = useAbsoluteUrl()
 
   return (
-    <div className={`flex items-center space-x-4 opacity-80 ${className}`}>
+    <div className={`flex items-center ${className}`}>
       <p>
         <i>Del: </i>
       </p>
@@ -19,7 +20,15 @@ export function ShareButtons({ className }: { className?: string }) {
         rel="noreferrer"
         aria-label="Del på Twitter"
       >
-        <FontAwesomeIcon icon={faSquareXTwitter} color="#000000" size="2x" />
+        <span className="fa-stack">
+          <FontAwesomeIcon className="fa-stack-1x" icon={faSquare} color="#FFFFFF" size="2x" />
+          <FontAwesomeIcon
+            className="fa-stack-1x"
+            icon={faSquareXTwitter}
+            color="#000000"
+            size="2x"
+          />
+        </span>
       </a>
       <a
         href={`https://linkedin.com/share?url=${url}`}
@@ -28,7 +37,10 @@ export function ShareButtons({ className }: { className?: string }) {
         rel="noreferrer"
         aria-label="Del på LinkedIn"
       >
-        <FontAwesomeIcon icon={faLinkedin} color="#0072b1" size="2x" />
+        <span className="fa-stack">
+          <FontAwesomeIcon className="fa-stack-1x" icon={faSquare} color="#FFFFFF" size="2x" />
+          <FontAwesomeIcon className="fa-stack-1x" icon={faLinkedin} color="#0072b1" size="2x" />
+        </span>
       </a>
     </div>
   )
