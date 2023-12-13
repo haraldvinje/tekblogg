a5b5c84b2f40c1d987e91ea3b4d4f1ea193a74c2
Harald Vinje <haraldvinje@gmail.com>
2023-12-13 17:19:10 +0100

    Change pwa provider

---
+++ tekblogg-web/next.config.mjs
@@ -0,0 +1,41 @@
+/** @type {import('next').NextConfig} */
+import withSerwistInit from "@serwist/next"
+import withBundleAnalyzerInit from '@next/bundle-analyzer'
+
+const withSerwist = withSerwistInit({
+  cacheOnFrontEndNav: true,
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+})
+
+const withBundleAnalyzer = withBundleAnalyzerInit({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nextjs.gallery',
+      }
+    ]
+  },
+  compiler: {
+    styledComponents: true
+  }
+}
+
+const finalConfig = (_phase, { }) => {
+  const plugins = [withSerwist, withBundleAnalyzer]
+  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig })
+}
+
+
+export default finalConfig
