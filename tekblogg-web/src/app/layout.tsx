39158379a0e5694637680034f1521228d1096160
Harald Vinje <haraldvinje@gmail.com>
2024-02-07 09:30:55 +0100

    Change gtm script to load after interactive to boost performance

--- tekblogg-web/src/app/layout.tsx
+++ tekblogg-web/src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="nb" className={inter.className}>
       <body className="transition duration-500 dark:bg-dark-lighter">
-        <Script id="gtmScript" nonce={nonce} strategy="beforeInteractive">
+        <Script id="gtmScript" nonce={nonce} strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
