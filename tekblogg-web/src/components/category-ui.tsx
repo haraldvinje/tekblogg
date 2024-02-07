ab0e2d7aa0270dfc4717bda76706a08e2fedafa7
Harald Vinje <haraldvinje@gmail.com>
2024-02-07 11:23:33 +0100

    Quickfix

--- tekblogg-web/src/components/category-ui.tsx
+++ tekblogg-web/src/components/category-ui.tsx
@@ -13,7 +13,7 @@ export function CategoryUi({
   const styles = `mb-2 mr-2 inline-block rounded-md bg-slate-100
     px-3 py-1 text-xs font-semibold text-gray-700 sm:text-sm
     ${isSelected ? 'bg-slate-400' : ''}`
-  const props = isButton ? { className: styles, onClick } : { onClick }
+  const props = isButton ? { className: styles, onClick } : { classNames: styles }
 
   return <Tag {...props}>{value}</Tag>
 }
