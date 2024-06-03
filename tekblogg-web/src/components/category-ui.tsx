e9545876305780be586cf2f4e5522e3fed0c8165
Harald Vinje <haraldvinje@gmail.com>
2024-06-03 15:02:42 +0200

    Small style adjustment

--- tekblogg-web/src/components/category-ui.tsx
+++ tekblogg-web/src/components/category-ui.tsx
@@ -10,7 +10,7 @@ export function CategoryUi({
   const isButton = onClick !== undefined
   const Tag = isButton ? 'button' : 'span'
 
-  const styles = `mb-2 mr-2 inline-block rounded-md bg-slate-100
+  const styles = `mb-2 mr-2 inline-block rounded-md bg-slate-200
     px-3 py-1 text-xs font-semibold text-gray-700 sm:text-sm
     ${isSelected ? 'bg-slate-400' : ''}`
   const props = isButton ? { className: styles, onClick } : { className: styles }
