1822264e723baf94da1571281f93f1ba11be6bd9
Harald Vinje <haraldvinje@gmail.com>
2025-11-14 11:09:02 +0100

    wip debug 418

--- tekblogg-web/src/app/page.tsx
+++ tekblogg-web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Home } from "./home";
 import { AnimationWrapper } from "@/components/animation-wrapper";
+import { FrontPageDebug } from "@/components/front-page-debug";
 import { getAllBlogPostsCardData } from "@/lib/sanity-client";
 
 export const revalidate = 60;
@@ -7,8 +8,9 @@ export const revalidate = 60;
 export default async function Page() {
   const posts = await getAllBlogPostsCardData();
   return (
-    <AnimationWrapper>
+    <>
+      <FrontPageDebug />
       <Home blogPostCards={posts} />
-    </AnimationWrapper>
+    </>
   );
 }
