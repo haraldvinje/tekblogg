f5b32812bcbc9d8389ca2b676028103a051add02
Harald <harald.vinje@schibsted.com>
2025-12-09 09:44:09 +0100

    remove preload of fonts

--- tekblogg-web/src/app/layout.tsx
+++ tekblogg-web/src/app/layout.tsx
@@ -13,11 +13,13 @@ const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
+  preload: false,
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+  preload: false,
 });
 
 const title = "TekBlogg";
